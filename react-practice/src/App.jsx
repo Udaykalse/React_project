0ad14dc6f2b3ca5b65c59403a32842cbe7a9c1e0
Udaykalse <udaykalse123@gmail.com>
2025-10-22 22:36:14 +0530

    ./

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,4 +1,8 @@
-
+import Button from "./components/buttons/button"
+import Form from "./components/testForm/form"
+import API from './components/testAPI/testapi';
+import Table from "./components/testTable/table";
+import List from "./components/testTodo/list";
 
 
 function App() {
@@ -6,7 +10,11 @@ function App() {
   return (
     <>
       Hello
-
+      <Button />
+      <Form />
+      <API />
+      <Table/>
+      <List/>
     </>
   )
 }
